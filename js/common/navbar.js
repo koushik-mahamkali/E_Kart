a9142f6cb7136caee1eadf27a6e51cb34bab615b
Koushik Mahamkali <mahamkalikoushik0@gmail.com>
2026-01-02 22:12:37 +0530

    structure

--- js/common/navbar.js
+++ js/common/navbar.js
@@ -2,24 +2,33 @@ function initNavbar() {
     const city = "Vijayawada";
     const pincode = "520007";
     const userName = "Koushik";
-
     const addressEl = document.getElementById("address");
     const loginEl = document.getElementById("login");
     const addressBtn = document.getElementById("addressBtn");
-
+    const popup = document.getElementById("popup");
+    const overlay = document.getElementById("overlay");
+    const closeBtn = document.getElementById("close");
     if (addressEl) {
         addressEl.innerText = city && pincode
             ? `${city} ${pincode}`
             : "Select Address";
     }
-
     if (loginEl) {
         loginEl.innerText = userName || "Login";
     }
-
-    if (addressBtn) {
+    if (addressBtn && popup && overlay) {
         addressBtn.addEventListener("click", () => {
-            alert("Open address selection modal here");
+            popup.hidden = false;
+            overlay.hidden = false;
         });
     }
+    if (closeBtn && popup && overlay) {
+        closeBtn.addEventListener("click", closeModal);
+        overlay.addEventListener("click", closeModal);
+    }
+    function closeModal() {
+        popup.hidden = true;
+        overlay.hidden = true;
+    }
 }
+document.addEventListener("DOMContentLoaded", initNavbar);
